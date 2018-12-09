7e1705a4de0750421b00b406d2aa17834beb9234
JEEJ <chevron@ecole.ensicaen.fr>
2018-12-09 11:06:18 +0100

    IHM : Init Javafx project

---
+++ src/main/java/fr/ensicaen/ecole/oasmr/app/Main.java
@@ -0,0 +1,25 @@
+package fr.ensicaen.ecole.oasmr.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Hello World!");
+        Parent root = FXMLLoader.load(getClass().getResource("/fr/ensicaen/ecole/oasmr/app/Main.fxml"));
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
