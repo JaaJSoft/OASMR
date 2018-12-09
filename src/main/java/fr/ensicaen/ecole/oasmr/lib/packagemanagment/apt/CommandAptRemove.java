f16ee6481d8208c14dca031f01641f17ea276f03
cuissot <harrypotter9752@gmail.com>
2018-12-09 17:36:21 +0100

    apt remove, test incomplet

--- src/main/java/fr/ensicaen/ecole/oasmr/lib/packagemanagment/apt/CommandAptRemove.java
+++ src/main/java/fr/ensicaen/ecole/oasmr/lib/packagemanagment/apt/CommandAptRemove.java
@@ -2,33 +2,30 @@ package fr.ensicaen.ecole.oasmr.lib.packagemanagment.apt;
 
 import fr.ensicaen.ecole.oasmr.lib.ProcessBuilderUtil;
 import fr.ensicaen.ecole.oasmr.lib.command.Command;
-import fr.ensicaen.ecole.oasmr.lib.packagemanagment.apt.exceptions.ExceptionAptInstallFailure;
+import fr.ensicaen.ecole.oasmr.lib.packagemanagment.apt.exceptions.ExceptionAptRemoveInexistantPackage;
 
 import java.io.IOException;
 import java.io.Serializable;
 
-public class CommandAptInstall extends Command {
-
-
+public class CommandAptRemove extends Command {
     private String packageName;
 
-    public CommandAptInstall(String packageName) {
-        this.packageName = packageName;
+    public CommandAptRemove(String packageName) {
+        this.packageName=packageName;
     }
 
     @Override
     public Serializable execute(Object... params) throws Exception {
-        ProcessBuilder processBuilder = new ProcessBuilder("sudo", "apt", "install", "-y", packageName);
+        ProcessBuilder processBuilder = new ProcessBuilder("sudo", "apt", "remove", "-y", packageName);
         try {
             Process p = processBuilder.start();
             p.waitFor();
             int ret = p.exitValue();
             switch (ret) {
                 case 100:
-                    throw new ExceptionAptInstallFailure(ProcessBuilderUtil.getOutputError(p));
+                    throw new ExceptionAptRemoveInexistantPackage(ProcessBuilderUtil.getOutputError(p));
                 default:
                     return ProcessBuilderUtil.getOutput(p);
-                //Il peut ne pas y avoir de résultat à la recherche meme si ça se passe bien
             }
 
         } catch (IOException | InterruptedException e) {
@@ -39,6 +36,6 @@ public class CommandAptInstall extends Command {
 
     @Override
     public String toString() {
-        return "sudo" + " " + "apt" + " " + "install" + " " + packageName;
+        return "sudo" + " " +"apt" + " " + "remove" + " " + packageName;
     }
 }
---
+++ src/test/java/fr/ensicaen/ecole/oasmr/lib/packagemanagment/apt/CommandAptRemoveTest.java
@@ -0,0 +1,21 @@
+package fr.ensicaen.ecole.oasmr.lib.packagemanagment.apt;
+
+import fr.ensicaen.ecole.oasmr.lib.packagemanagment.apt.exceptions.ExceptionAptRemoveInexistantPackage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CommandAptRemoveTest {
+    @Test
+    public void execute() throws Exception{
+        CommandAptShow c = new CommandAptShow("apcalc");
+        assertEquals(""/*To complete*/, c.execute());
+    }
+
+
+    @Test(expected = ExceptionAptRemoveInexistantPackage.class)
+    public void executeFailure() throws Exception{
+        CommandAptShow c = new CommandAptShow("fuufeaofjeej");
+    }
+
+}
