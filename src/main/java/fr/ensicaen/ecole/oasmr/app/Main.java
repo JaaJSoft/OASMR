39e22f63e776e5b4a98a10dc89d68fb8ac6551a1
JulienChevron <chevron-j@hotmail.fr>
2018-12-11 00:07:15 +0100

    IHM : Add ControlsFX into gradle

--- src/main/java/fr/ensicaen/ecole/oasmr/app/Main.java
+++ src/main/java/fr/ensicaen/ecole/oasmr/app/Main.java
@@ -2,7 +2,6 @@ package fr.ensicaen.ecole.oasmr.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
