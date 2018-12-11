20ab471f1d0443b46503459e13c5eabc83f01796
pchopinet <pierre.chopinet97@gmail.com>
2018-12-11 10:25:42 +0100

    RequestManagerFlyweight : update test II

--- src/test/java/fr/ensicaen/ecole/oasmr/supervisor/request/RequestManagerFlyweightFactoryTest.java
+++ src/test/java/fr/ensicaen/ecole/oasmr/supervisor/request/RequestManagerFlyweightFactoryTest.java
@@ -10,22 +10,35 @@ import java.net.UnknownHostException;
 import static org.junit.Assert.*;
 
 public class RequestManagerFlyweightFactoryTest {
+    private RequestManagerFlyweightFactory factory;
+
+    @Before
+    public void setUp() throws Exception {
+        factory = RequestManagerFlyweightFactory.getInstance();
+        factory.clear();
+    }
 
     @Test
     public void getRequestManager() throws UnknownHostException, ExceptionPortInvalid {
-        RequestManagerFlyweightFactory factory = RequestManagerFlyweightFactory.getInstance();
         RequestManager r = factory.getRequestManager(InetAddress.getByName("192.168.1.1"), 55555);
         assertSame(r, factory.getRequestManager(InetAddress.getByName("192.168.1.1"), 55555));
     }
 
     @Test
     public void getRequestManagerSize() throws UnknownHostException, ExceptionPortInvalid {
-        RequestManagerFlyweightFactory factory = RequestManagerFlyweightFactory.getInstance();
-        factory.clear();
         RequestManager r = factory.getRequestManager(InetAddress.getByName("192.168.1.2"), 55555);
         RequestManager r2 = factory.getRequestManager(InetAddress.getByName("192.168.1.2"), 55555);
 
         assertEquals(1, factory.getNbRequestManager());
+
+    }
+
+    @Test
+    public void getRequestManagerSizeTwoInsertion() throws UnknownHostException, ExceptionPortInvalid {
+        RequestManager r = factory.getRequestManager(InetAddress.getByName("192.168.1.1"), 55555);
+        RequestManager r2 = factory.getRequestManager(InetAddress.getByName("192.168.1.2"), 55555);
+
+        assertEquals(2, factory.getNbRequestManager());
 
     }
 
