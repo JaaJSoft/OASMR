114414f5f95f18f0cb2a6644c6241909c290a76e
Pierre Chopinet <pierre.chopinet97@gmail.com>
2018-12-23 11:29:45 +0100

    CLI : get list of node

--- src/main/java/fr/ensicaen/ecole/oasmr/cli/MainCli.java
+++ src/main/java/fr/ensicaen/ecole/oasmr/cli/MainCli.java
@@ -1,45 +1,29 @@
 package fr.ensicaen.ecole.oasmr.cli;
 
-import fr.ensicaen.ecole.oasmr.lib.example.CommandEchoString;
-import fr.ensicaen.ecole.oasmr.lib.network.exception.ExceptionConnectionFailure;
-import fr.ensicaen.ecole.oasmr.lib.network.exception.ExceptionPortInvalid;
 import fr.ensicaen.ecole.oasmr.supervisor.node.Node;
-import fr.ensicaen.ecole.oasmr.supervisor.node.request.RequestExecuteCommand;
 import fr.ensicaen.ecole.oasmr.supervisor.node.request.RequestGetNodes;
 import fr.ensicaen.ecole.oasmr.supervisor.request.RequestManager;
+import fr.ensicaen.ecole.oasmr.supervisor.request.RequestManagerFlyweightFactory;
+import picocli.CommandLine;
 
-import java.net.InetAddress;
 import java.util.Set;
 
 class MainCli {
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
-            System.out.println("Usage <IPAddress> <port>");
-            return;
-        }
-        String ip = args[0];
-        int port = Integer.parseInt(args[1]);
-        try {
-            RequestManager r;
-            r = new RequestManager(InetAddress.getByName(ip), port);
-
-            @SuppressWarnings("unchecked")
+        SupervisorCli s = new SupervisorCli();
+        CommandLine cmd = new CommandLine(s);
+        cmd.parse(args);
+        if (cmd.isUsageHelpRequested()) {
+            cmd.usage(System.out);
+        } else if (cmd.isVersionHelpRequested()) {
+            cmd.printVersionHelp(System.out);
+        } else {
+            RequestManager r = RequestManagerFlyweightFactory.getInstance().getRequestManager(s.supervisorAddress, s.port);
             Set<Node> nodes = (Set<Node>) r.sendRequest(new RequestGetNodes());
-
-            for (Node n : nodes) {
-                System.out.println(n);
-            }
-            System.out.println(r.sendRequest(new RequestExecuteCommand(nodes.iterator().next().getId(), new CommandEchoString("jeej"))));
-
-        } catch (ExceptionPortInvalid exceptionPortInvalid) {
-            System.out.println("Port invalid : " + port);
-        } catch (ExceptionConnectionFailure e) {
-            System.out.println("Server not found");
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
+            System.out.println(nodes);
         }
+
     }
 
 }
