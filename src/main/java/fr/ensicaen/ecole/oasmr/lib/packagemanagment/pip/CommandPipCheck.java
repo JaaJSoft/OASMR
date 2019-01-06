f3fd0af14a883362491da6af53337ea0c4dfad33
Paul Freifer <45093492+freifer@users.noreply.github.com>
2019-01-06 17:59:55 +0100

    Add files via upload

---
+++ src/main/java/fr/ensicaen/ecole/oasmr/lib/packagemanagment/pip/CommandPipCheck.java
@@ -0,0 +1,42 @@
+
+package fr.ensicaen.ecole.oasmr.lib.packagemanagment.pip;
+
+import fr.ensicaen.ecole.oasmr.lib.ProcessBuilderUtil;
+import fr.ensicaen.ecole.oasmr.lib.command.Command;
+import fr.ensicaen.ecole.oasmr.lib.packagemanagment.pip.exceptions.PipException;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class CommandPipCheck extends Command {
+
+    public CommandPipCheck() {}
+
+    @Override
+    public Serializable execute(Object... params) throws Exception {
+        ProcessBuilder processBuilder = new ProcessBuilder("pip", "check");
+        try {
+            Process p = processBuilder.start();
+            p.waitFor();
+            int ret = p.exitValue();
+            switch (ret) {
+                case 0:
+                    //System.out.println(ProcessBuilderUtil.getOutput(p));
+                    return ProcessBuilderUtil.getOutput(p);
+
+                default:
+                    throw new PipException(ProcessBuilderUtil.getOutputError(p));
+
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return e;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "pip" + " " + "check";
+    }
+}
---
+++ src/main/java/fr/ensicaen/ecole/oasmr/lib/packagemanagment/pip/CommandPipDownload.java
@@ -0,0 +1,44 @@
+
+package fr.ensicaen.ecole.oasmr.lib.packagemanagment.pip;
+
+import fr.ensicaen.ecole.oasmr.lib.ProcessBuilderUtil;
+import fr.ensicaen.ecole.oasmr.lib.command.Command;
+import fr.ensicaen.ecole.oasmr.lib.packagemanagment.pip.exceptions.PipException;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class CommandPipDownload extends Command {
+
+    private String packageName;
+
+    public CommandPipDownload(String packageName) { this.packageName = packageName; }
+
+    @Override
+    public Serializable execute(Object... params) throws Exception {
+        ProcessBuilder processBuilder = new ProcessBuilder("pip", "download", packageName);
+        try {
+            Process p = processBuilder.start();
+            p.waitFor();
+            int ret = p.exitValue();
+            switch (ret) {
+                case 0:
+                    //System.out.println(ProcessBuilderUtil.getOutput(p));
+                    return ProcessBuilderUtil.getOutput(p);
+
+                default:
+                    throw new PipException(ProcessBuilderUtil.getOutputError(p));
+
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return e;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "pip" + " " + "download" + " " + packageName;
+    }
+}
