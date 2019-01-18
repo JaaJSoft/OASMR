22712a009245a24c500a80e5f5978a03a6483152
JulienChevron <chevron-j@hotmail.fr>
2019-01-18 17:44:25 +0100

    IHM : update

--- src/main/java/fr/ensicaen/ecole/oasmr/app/controller/GroupViewController.java
+++ src/main/java/fr/ensicaen/ecole/oasmr/app/controller/GroupViewController.java
@@ -1,9 +1,13 @@
 package fr.ensicaen.ecole.oasmr.app.controller;
 
+import com.jfoenix.controls.JFXTabPane;
 import fr.ensicaen.ecole.oasmr.app.beans.NodeBean;
 import fr.ensicaen.ecole.oasmr.app.view.DataModel;
+import fr.ensicaen.ecole.oasmr.supervisor.request.RequestManager;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -17,7 +21,20 @@ public class GroupViewController implements Initializable {
     @FXML
     Text nodeId;
 
+    @FXML
+    JFXTabPane moduleTabPane;
+
+    @FXML
+    VBox rightVBox;
+
+    @FXML
+    VBox mainVBox;
+
+    @FXML
+    AnchorPane bottomPane;
+
     private DataModel model;
+    private RequestManager requestManager;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -28,12 +45,35 @@ public class GroupViewController implements Initializable {
         model = dataModel;
     }
 
+    public void setRequestManager(RequestManager rm) {
+        requestManager = rm;
+    }
+
+    //TODO : fill with good infos
     public void update(){
-        //TODO : give good infos
+        updateNodesInfo();
+        updateModuleTab();
+        updateNodeTerm();
+        updateRightInfo();
+    }
+
+    private void updateNodesInfo(){
         nodeName.setText("Group : ");
         for(NodeBean node : model.getCurrentNodeBeans()) {
             nodeName.setText(nodeName.getText() + node.toString() + " ");
         }
+    }
+
+    private void updateModuleTab() {
+
+    }
+
+    private void updateNodeTerm() {
+
+    }
+
+    private void updateRightInfo() {
+
     }
 
 }
--- src/main/java/fr/ensicaen/ecole/oasmr/app/controller/NodeViewController.java
+++ src/main/java/fr/ensicaen/ecole/oasmr/app/controller/NodeViewController.java
@@ -1,10 +1,17 @@
 package fr.ensicaen.ecole.oasmr.app.controller;
 
+import com.jfoenix.controls.JFXTabPane;
 import fr.ensicaen.ecole.oasmr.app.beans.NodeBean;
 import fr.ensicaen.ecole.oasmr.app.view.DataModel;
+import fr.ensicaen.ecole.oasmr.supervisor.request.RequestManager;
 import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -18,7 +25,20 @@ public class NodeViewController implements Initializable {
     @FXML
     Text nodeId;
 
+    @FXML
+    JFXTabPane moduleTabPane;
+
+    @FXML
+    VBox rightVBox;
+
+    @FXML
+    VBox mainVBox;
+
+    @FXML
+    AnchorPane bottomPane;
+
     private DataModel model;
+    private RequestManager requestManager;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -29,15 +49,49 @@ public class NodeViewController implements Initializable {
         model = dataModel;
     }
 
-    public void update(){
-        //TODO : give goood infos
-        if(model.getSelectedAmount() == 1){
-            nodeName.setText(model.getCurrentNodeBeans().get(0).toString());
-            nodeId.setText(String.valueOf(model.getCurrentNodeBeans().get(0).getId()));
-        }else{
-            nodeName.setText("No node selected");
-            nodeId.setText("");
-        }
+    public void setRequestManager(RequestManager rm) {
+        requestManager = rm;
     }
+
+    //TODO : fill with good infos
+    public void update(){
+        updateNodeInfo();
+        updateModuleTab();
+        updateNodeTerm();
+        updateRightInfo();
+    }
+
+    private void updateNodeInfo(){
+        nodeName.setText(model.getCurrentNodeBeans().get(0).toString());
+        nodeId.setText(String.valueOf(model.getCurrentNodeBeans().get(0).getId()));
+    }
+
+
+    private void updateModuleTab(){
+        moduleTabPane.getTabs().clear();
+        Tab t = new Tab();
+        t.setText("Module 1");
+        t.setContent(new Label("Insert module core"));
+        Tab t2 = new Tab();
+        t2.setText("Module 2");
+        t2.setContent(new Label("Insert module core"));
+        moduleTabPane.getTabs().addAll(t, t2);
+    }
+
+    private void updateNodeTerm(){
+        bottomPane.getChildren().clear();
+        bottomPane.getChildren().add(new Label("Term"));
+    }
+
+    private void updateRightInfo(){
+        rightVBox.getChildren().clear();
+        rightVBox.getChildren().add(new Label("CPU/RAM usage"));
+        rightVBox.getChildren().add(new Separator());
+        rightVBox.getChildren().add(new Label("Files"));
+
+    }
+
+
+
 
 }
