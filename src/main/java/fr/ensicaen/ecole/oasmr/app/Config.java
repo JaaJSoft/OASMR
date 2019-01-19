1e354518ea9f9133b9dfcec7dd30a4c45c83923c
pchopinet <pierre.chopinet97@gmail.com>
2019-01-19 17:39:35 +0100

    ihm : create config

---
+++ src/main/java/fr/ensicaen/ecole/oasmr/app/Config.java
@@ -0,0 +1,10 @@
+package fr.ensicaen.ecole.oasmr.app;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Config { //TODO FIND A CLEANER SOLUTION
+    public static String ip = "127.0.0.1";
+
+    public static int port = 40404;
+}
