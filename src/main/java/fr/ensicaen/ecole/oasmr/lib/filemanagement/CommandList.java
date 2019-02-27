667fa0791127c7a3698097c27d39d28c4bab2110
RemiLecouillard <remi@lecouillard.org>
2019-02-27 15:58:43 +0100

    Adds CommandList implementation

--- src/main/java/fr/ensicaen/ecole/oasmr/lib/filemanagement/CommandList.java
+++ src/main/java/fr/ensicaen/ecole/oasmr/lib/filemanagement/CommandList.java
@@ -2,15 +2,33 @@ package fr.ensicaen.ecole.oasmr.lib.filemanagement;
 
 import fr.ensicaen.ecole.oasmr.lib.command.Command;
 
+import java.io.File;
 import java.io.Serializable;
 
 /**
- * Lists all the files.
+ * Lists all the files of a directory.
+ * @TODO Add a filter
  */
 public class CommandList extends Command {
+
+    /**
+     * The directory path name to list.
+     */
+    private String directoryPathname;
+
+    /**
+     *
+     * @param directoryPathname The directory path name to list.
+     */
+    public CommandList(String directoryPathname) {
+        this.directoryPathname = directoryPathname;
+    }
+
     @Override
     protected Serializable execute(Object... params) throws Exception {
-        return null;
+        File file = new File(directoryPathname);
+
+        return file.list();
     }
 
     @Override
