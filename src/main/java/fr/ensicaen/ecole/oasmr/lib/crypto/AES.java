6912e3d64e567d8819a1491d021e3c2d0618e14a
FREIFER <paul.freifer@hotmail.com>
2019-03-11 22:46:49 +0100

    AES: setKey function

---
+++ src/main/java/fr/ensicaen/ecole/oasmr/lib/crypto/AES.java
@@ -0,0 +1,31 @@
+package fr.ensicaen.ecole.oasmr.lib.crypto;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class AES {
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+
+    public static void setKey(String myKey)
+    {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
