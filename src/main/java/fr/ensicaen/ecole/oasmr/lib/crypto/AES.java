3bea88d3a4bfd9e784b57f27be7b1566152aae6b
FREIFER <paul.freifer@hotmail.com>
2019-03-11 22:53:04 +0100

    AES: encrypt function

--- src/main/java/fr/ensicaen/ecole/oasmr/lib/crypto/AES.java
+++ src/main/java/fr/ensicaen/ecole/oasmr/lib/crypto/AES.java
@@ -1,10 +1,12 @@
 package fr.ensicaen.ecole.oasmr.lib.crypto;
 
+import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class AES {
     private static SecretKeySpec secretKey;
@@ -26,6 +28,22 @@ public class AES {
         catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String encrypt(String strToEncrypt, String secret)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
     }
 }
 
