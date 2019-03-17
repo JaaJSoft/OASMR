27600dcd1f8cd9b610278610b78644bc74b44b1e
FREIFER <paul.freifer@hotmail.com>
2019-03-17 18:37:38 +0100

    minor changes to AES functions

--- src/main/java/fr/ensicaen/ecole/oasmr/lib/crypto/AES.java
+++ src/main/java/fr/ensicaen/ecole/oasmr/lib/crypto/AES.java
@@ -2,7 +2,7 @@ package fr.ensicaen.ecole.oasmr.lib.crypto;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -12,12 +12,12 @@ public class AES {
     private static SecretKeySpec secretKey;
     private static byte[] key;
 
-    public static void setKey(String myKey)
+    private static void setKey(String myKey)
     {
         MessageDigest sha = null;
         try {
-            key = myKey.getBytes("UTF-8");
-            sha = MessageDigest.getInstance("SHA-1");
+            key = myKey.getBytes(StandardCharsets.UTF_8);
+            sha = MessageDigest.getInstance("SHA-256");
             key = sha.digest(key);
             key = Arrays.copyOf(key, 16);
             secretKey = new SecretKeySpec(key, "AES");
@@ -25,22 +25,17 @@ public class AES {
         catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
     }
 
     public static String encrypt(String strToEncrypt, String secret)
     {
-        try
-        {
+        try {
             setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             System.out.println("Error while encrypting: " + e.toString());
         }
         return null;
@@ -48,15 +43,13 @@ public class AES {
 
     public static String decrypt(String strToDecrypt, String secret)
     {
-        try
-        {
+        try {
             setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             System.out.println("Error while decrypting: " + e.toString());
         }
         return null;
