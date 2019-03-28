ce917eea6848341d74fd0adbdea9913326832e13
aCuissot <harrypotter9752@gmail.com>
2019-03-28 10:22:55 +0100

    JAAAAAAAAAAAAAAAJ Recherche commande

--- src/main/java/fr/ensicaen/ecole/oasmr/app/controller/node/NodeCommandController.java
+++ src/main/java/fr/ensicaen/ecole/oasmr/app/controller/node/NodeCommandController.java
@@ -38,12 +38,22 @@ import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class NodeCommandController extends View {
 
     @FXML
+    private
     FlowPane commandFlowPane;
+
+    @FXML
+    private
+    JFXButton searchBtn;
+
+    @FXML
+    private
+    JFXTextField searchField;
 
     private RequestManager requestManager = null;
     private Config config;
@@ -57,6 +67,29 @@ public class NodeCommandController extends View {
     @Override
     public void onCreate() {
         nodesModel = NodesModel.getInstance();
+        searchBtn.setOnAction(event -> {
+
+            try {
+                jeeeeeeeeeeej(searchField.getText());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+
+        searchField.setOnAction(event -> {
+            try {
+                jeeeeeeeeeeej(searchField.getText());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+
     }
 
     @Override
@@ -76,13 +109,7 @@ public class NodeCommandController extends View {
             //TODO : Configure view for group
         } else if (nodesModel.getSelectedAmount() == 1) {
             try {
-                commandFlowPane.getChildren().clear();
-                Future<? extends Serializable> reponseCommandList = requestManager.aSyncSendRequest(new RequestGetCommands());
-                Set<Class<? extends Command>> commands = (Set<Class<? extends Command>>) reponseCommandList.get();
-                for (Class<? extends Command> command : commands) {
-                    JFXButton jeej = initButtonFromClass(command);
-                    commandFlowPane.getChildren().add(jeej);
-                }
+                jeeeeeeeeeeej("");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -90,8 +117,29 @@ public class NodeCommandController extends View {
 
     }
 
+    private void jeeeeeeeeeeej(String search) throws InterruptedException, java.util.concurrent.ExecutionException {
+
+        commandFlowPane.getChildren().clear();
+        Future<? extends Serializable> reponseCommandList = requestManager.aSyncSendRequest(new RequestGetCommands());
+        Set<Class<? extends Command>> commands = (Set<Class<? extends Command>>) reponseCommandList.get();
+        if (search.trim().equals("")) {
+            for (Class<? extends Command> command : commands) {
+                JFXButton jeej = initButtonFromClass(command);
+                commandFlowPane.getChildren().add(jeej);
+            }
+        } else {
+            for (Class<? extends Command> command : commands) {
+                if (command.getSimpleName().split("Command")[1].toLowerCase().contains(search.toLowerCase())) {
+                    JFXButton jeej = initButtonFromClass(command);
+                    commandFlowPane.getChildren().add(jeej);
+                }
+            }
+        }
+    }
+
+
     private JFXButton initButtonFromClass(Class<? extends Command> command) {
-        JFXButton jeej = new JFXButton(command.getSimpleName());
+        JFXButton jeej = new JFXButton(command.getSimpleName().split("Command")[1]);
         jeej.setStyle("-jfx-button-type: RAISED;-fx-background-color: #FF6026; -fx-text-fill: white;");
         jeej.setOnAction(e -> {
             Stage stage = (Stage) commandFlowPane.getScene().getWindow();
